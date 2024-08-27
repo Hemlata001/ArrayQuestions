3f7e2e164627e6c6ceb8d70fe6c017c0f52113a4
Hemlata <127768311+Hemlata001@users.noreply.github.com>
2024-08-27 12:50:41 +0530

    Add files via upload

---
+++ ImportantArray/Array2D/StaircaseSearch.java
@@ -0,0 +1,32 @@
+package Array2D;
+import java.util.*;
+public class StaircaseSearch
+{
+    public static boolean staircaseSearch(int[][] matrix,int key){
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col >= 0){
+            if(matrix[row][col]==key){
+                System.out.println("found key at (" + row + "," + col + ")");
+                return true;
+            }
+            else if(key<matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        System.out.println("Key is not found");
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        //int[][] result = staircaseSearch(matrix,33);
+        //System.out.println(result);
+        System.out.println("Staircase Search : " + staircaseSearch(matrix,33));
+}
+}
+// Time Complexity : O(n+m)
+// n = row;
+// m = col;
+// Space Complexity : O(1)
